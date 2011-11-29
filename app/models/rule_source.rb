e284e01ca68935dadb559e3b42bc3c7315d92205
nebrera <pablo@nebrera.com>
2011-11-29 12:27:03 +0100

    New columns in rule source model: md5 and timestamp. Modifications rules2rbIPS

--- app/models/rule_source.rb
+++ app/models/rule_source.rb
@@ -7,6 +7,8 @@ class RuleSource
   property :id, Serial, :key => true, :index => true
   property :name, String, :length => 64, :required => true
   property :description, String, :length => 128
+  property :timestamp, DateTime, :index => true
+  property :md5, String, :length => 64  
 
   has n, :rules, :child_key => [ :source_id ], :constraint => :destroy
 
